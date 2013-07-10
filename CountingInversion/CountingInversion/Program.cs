3cd7f76c681dde0af0f1620a9a91f61b35cc6388
kjakimiec <krzysztof.jakimiec@gmail.com>
2013-07-10 21:39:42 +0200

    "algo pq1"

---
+++ CountingInversion/CountingInversion/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace CountingInversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = new FileReader();
+            file.ReadToArray("numbers.txt");
+            file.ListArray();
+        }
+    }
+
+
+
+    public class Inversion
+    {
+        public int GetSplitPLace(int size)
+        {
+            return (int)(size / 2);
+        }
+
+        public int CountInversion(IList<int> source,int from, int to)
+        {
+            var split = GetSplitPLace(to-from);
+            var left = CountInversion(source, 0, split);
+            var right = CountInversion(source, split, to);
+            var splited = CountSplited();
+            return left+right+splited;
+        }
+
+        private int CountSplited()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+
+
+    public class FileReader
+    {
+        List<int> array = new List<int>();
+
+
+        public void ReadToArray(string fileName)
+        {
+            var fileDesc = File.OpenText(fileName);
+            string line;
+            while ((line = fileDesc.ReadLine()) != null)
+            {
+                array.Add(int.Parse(line));
+            }
+
+            fileDesc.Close();
+        }
+
+        public void ListArray()
+        {
+            foreach (var i in array)
+                System.Console.WriteLine(i);
+        }
+    }
+}
---
+++ CountingInversion/InversionTests/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CountingInversion;
+
+namespace InversionTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        public Inversion CreateInversion()
+        {
+            return new Inversion();
+        }
+
+        [TestMethod]
+        public void Inveresion_size2_result1()
+        {
+            var sut = CreateInversion();
+            Assert.AreEqual(1, sut.GetSplitPLace(2));
+        }
+
+        [TestMethod]
+        public void Inversion_size3_result2()
+        {
+            var sut = CreateInversion();
+            Assert.AreEqual(1, sut.GetSplitPLace(3)); 
+        }
+
+
+        [TestMethod]
+        public void Inversion_size13_result6()
+        {
+            var sut = CreateInversion();
+            Assert.AreEqual(6, sut.GetSplitPLace(13));
+        }
+
+
+    }
+}
